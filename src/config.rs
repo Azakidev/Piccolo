7f30cedaea2a8f9d4d066925ca4afd22abd054d0
Azakidev <zazaguichi@outlook.com>
2026-09-05 18:42:40 +0200

    Skrink icon again, fix license copyright and rename, tidy deps

--- src/config.rs
+++ src/config.rs
@@ -8,7 +8,7 @@
 pub static APP_ID: &str = default_env(option_env!("APP_ID"), "art.fatdawlf.Piccolo");
 pub static VERSION: &str = default_env(option_env!("VERSION"), "unknown");
 pub static GETTEXT_PACKAGE: &str = default_env(option_env!("GETTEXT_PACKAGE"), "piccolo");
-pub static LOCALEDIR: &str = default_env(option_env!("LOCALEDIR"), "/ap/share/locale");
+pub static LOCALEDIR: &str = default_env(option_env!("LOCALEDIR"), "/app/share/locale");
 pub static PKGDATADIR: &str = default_env(option_env!("PKGDATADIR"), "/app/share/piccolo");
 
 const fn default_env(v: Option<&'static str>, default: &'static str) -> &'static str {
